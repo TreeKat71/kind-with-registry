af7602c49902e45cb2bac082f17fe01ebdfd79e9
TreeKat <muller79924@gmail.com>
2021-12-27 01:04:12 +0800

    Fix: chmod kubectl executable

--- playground/all_in_one.sh
+++ playground/all_in_one.sh
@@ -1,6 +1,7 @@
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kind
+chmod +x ./kubectl
 mv kind /usr/bin/
 mv kubectl /usr/bin/
 git clone https://github.com/TreeKat71/sandbox-k8s-with-kind.git
